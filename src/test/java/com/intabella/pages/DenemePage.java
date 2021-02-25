f640c9af6097df362947a7e3a33405c4fc455680
Yalcin YILDIZ <64442783+yyildizz@users.noreply.github.com>
2021-02-25 22:15:11 +0200

    deneme page eklendi

---
+++ src/test/java/com/intabella/pages/DenemePage.java
@@ -0,0 +1,17 @@
+package com.intabella.pages;
+
+import com.intabella.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DenemePage {
+
+    public DenemePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+
+
+
+
+}
